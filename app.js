b95222abba72565ca724a78d562fb7f2908e192f
MaxTrusov <46316587+MaxTrusov@users.noreply.github.com>
2022-09-19 16:37:59 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-const slides = document.querySelectorAll('.slide');
 
-for(const slide of slides){
-   slide.addEventListener('click', () => {
-      clearActiveClasses();
-      slide.classList.add('active');
-   })
+function slidesPlagin(activeSlide) {
+   const slides = document.querySelectorAll('.slide');
+
+   slides[activeSlide].classList.add('active');
+
+   for (const slide of slides) {
+      slide.addEventListener('click', () => {
+         clearActiveClasses();
+         slide.classList.add('active');
+      })
+   }
+
+   function clearActiveClasses() {
+      slides.forEach((slide) => {
+         slide.classList.remove('active');
+      })
+   }
 }
 
-function clearActiveClasses() {
-   slides.forEach((slide) => {
-      slide.classList.remove('active');
-   })
-}
+slidesPlagin(0);
